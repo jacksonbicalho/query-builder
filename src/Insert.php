480fea1abc9b472ba5c5ffda8ee369025c59ef5e
Jackson Bicalho <jacksonbicalho@gmail.com>
2020-12-13 09:33:49 -0300

    primeiro commit

---
+++ src/Insert.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QueryBuilder;
+
+/**
+ * Inserindo comentário apenas para testes
+ */
+class Insert{
+
+    private $queryBuilt = null;
+
+    protected function getFields($arr): string
+    {
+        $_fields = '';
+        $fields = '';
+
+        if (isset($arr[0])) {
+            if (is_array($arr[0])) {
+                $_fields = array_map(function($element){
+                    return implode(', ', array_keys($element));
+                }, $arr);
+            }
+        }
+
+        if (is_array($_fields)) {
+            foreach ($_fields as $_field) {
+                if ($_field != $fields ) {
+                    $fields.= $_field;
+                }
+            }
+        }
+
+        if (empty($fields)) {
+            $fields .= implode(', ', array_keys($arr));
+        }
+
+        return $fields;
+    }
+
+    protected function getValues($arr): string
+    {
+        $values = '';
+
+        if (isset($arr[0])) {
+            if (is_array($arr[0])) {
+                $values = array_map(function($element){
+                    return '(' . implode(', ', array_values($element)) . ')';
+                }, $arr);
+            }
+            $values = implode(', ', array_values($values));
+        }
+
+        if (empty($values)) {
+            $values .= '(' . implode(', ', array_values($arr)) . ')';
+        }
+
+        return $values;
+    }
+
+    public function buildInsert(string $table, array $body): Insert
+    {
+        $insert = "INSERT INTO {$table}";
+        $fields = $this->getFields($body);
+        $values = $this->getValues($body);
+
+        $insert.= " ({$fields}) VALUES {$values};";
+
+        $this->queryBuilt = $insert;
+
+        return $this;
+    }
+
+    public function txt(){
+        return $this->queryBuilt;
+    }
+
+
+    public function teste(): string
+    {
+        return "Teste!";
+    }
+
+    public function teste3(): string
+    {
+        return "Teste!";
+    }
+
+
+
+
+
+
+}
---
+++ tests/unit/InsertTest.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+
+final class InsertTest extends TestCase
+{
+
+    protected $bQ = null;
+
+    protected function setUp()
+    {
+        $this->bQ = new QueryBuilder\Insert();
+    }
+
+    public function testBuildInsertSimple(): void
+    {
+        $sql = $this->bQ->buildInsert('produtos',
+            [
+                'id' => 1,
+                'nome' => "'aasasdasdas'",
+                'preco' => 1.20
+            ]
+
+        )->txt();
+
+        $this->assertEquals($sql
+        , "INSERT INTO produtos (id, nome, preco) VALUES (1, 'aasasdasdas', 1.2);");
+    }
+
+    public function testBuildInsertMultiple(): void
+    {
+        $sql = $this->bQ->buildInsert('produtos',[
+            [
+                'id' => 1,
+                'nome' => "'nome1'",
+                'preco' => 1.20
+            ],
+            [
+                'id' => 2,
+                'nome' => "'nome2'",
+                'preco' => 1.23
+            ],
+            [
+                'id' => 3,
+                'nome' => "'nome3'",
+                'preco' => 0.99
+            ]
+        ])->txt();
+
+        $this->assertEquals(
+            $sql,
+            "INSERT INTO produtos (id, nome, preco) VALUES (1, 'nome1', 1.2), (2, 'nome2', 1.23), (3, 'nome3', 0.99);"
+        );
+    }
+}
